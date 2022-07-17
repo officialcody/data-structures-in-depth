408869e1c773d32aa18be3539fafc35bf6fb5afc
officialcody <sparshgoelt26@gmail.com>
2022-07-17 22:13:30 +0530

    Print alternate numbers

---
+++ Day1/Ques1.java
@@ -0,0 +1,25 @@
+package Day1;
+
+import java.util.*;
+
+// Print alternate elements of an array
+
+public class Ques1 {
+
+    public static void printAlternateNumbers(int arr [], int n) {
+        for(int i=0;i<n;i=i+2){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[100];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        printAlternateNumbers(arr, n);
+        scan.close();
+    }
+}
