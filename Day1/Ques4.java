2ecb8db5559690954bf8947bf613cf4431b9f474
officialcody <sparshgoelt26@gmail.com>
2022-07-17 22:48:49 +0530

    Sum of elements of an array

---
+++ Day1/Ques4.java
@@ -0,0 +1,27 @@
+package Day1;
+
+import java.util.*;
+
+// Sum of elements in Array
+public class Ques4 {
+    public static long getSum(long a[], long n)
+    {
+        long sum = 0;
+        for(int number=0;number<n;number++){
+            sum += a[number];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextLong();
+        long arr[] = new long[100];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        long secondLargest = getSum(arr, n);
+        System.out.println("Sum of elements in array is: " + secondLargest);
+        scan.close();
+    }
+}
