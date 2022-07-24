786e09db096a3de8ff3a82b6ff843b1c412e38b9
officialcody <sparshgoelt26@gmail.com>
2022-07-24 15:25:47 +0530

    last index of 1 in string

---
+++ Day8/Ques6.java
@@ -0,0 +1,23 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class Ques6 {
+    public static int lastIndex(String s) {
+        int lastIndex = -1;
+        for(int i=0;i<s.length();i++){
+            if((char)s.charAt(i) == '1'){
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int lastIndexOf1 = lastIndex(s);
+        System.out.println("Last index of 1 in string " + s + " is: " + lastIndexOf1);
+        scan.close();
+    }
+}
