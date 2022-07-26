e5d279c44cb0ee1828d6ca634935c98093d0fb94
officialcody <sparshgoelt26@gmail.com>
2022-07-26 14:12:48 +0530

    power of a number raised to a number optimized

--- Recursion/PowerOfNumber.java
+++ Recursion/PowerOfNumber.java
@@ -3,16 +3,22 @@ package Recursion;
 import java.util.*;
 
 public class PowerOfNumber {
-    public static int power(int base, int power){
+    public static long power(int base, int power){
         if(power == 0) return 1;
         return base * power(base, power - 1);
+    }
+
+    public static long powerOptimized(int base, int power){
+        if(power == 0) return 1;
+        if(power%2 == 1) return power(base, power/2) * power(base, power/2) * base;
+        return power(base, power/2) * power(base, power/2);
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int base = scan.nextInt();
         int pow = scan.nextInt();
-        int power = power(base, pow);
+        long power = powerOptimized(base, pow);
         System.out.println("Value of " + base + " to the power of " + pow + " is: " + power);
         scan.close();
     }
