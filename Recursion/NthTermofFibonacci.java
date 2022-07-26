c227d24f161cd5850b18e203e1c65f4ca3fba769
officialcody <sparshgoelt26@gmail.com>
2022-07-26 13:48:27 +0530

    nth fibonacci number

---
+++ Recursion/NthTermofFibonacci.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.*;
+
+public class NthTermofFibonacci {
+    public static int nthFibonacci(int number){
+        if(number == 1|| number == 2) return 1;
+        return nthFibonacci(number-1) + nthFibonacci(number-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int nthFibonacci = nthFibonacci(n);
+        System.out.println("Factorial of " + n + " is: " + nthFibonacci);
+        scan.close();
+    }
+}
