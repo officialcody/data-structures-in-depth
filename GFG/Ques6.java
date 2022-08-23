b19d7e9f75272fa74bb376c1fadb31a6c56ff9d8
officialcody <sparshgoelt26@gmail.com>
2022-08-23 22:34:07 +0530

    Array Subset of another array

---
+++ GFG/Ques6.java
@@ -0,0 +1,88 @@
+package GFG;
+
+// Array Subset of another array
+import java.util.*;
+
+public class Ques6 {
+    /***
+     * USING ARRAYLIST
+     * @param a1
+     * @param a2
+     * @param n
+     * @param m
+     * @return
+     */
+    public static String isSubset2( long a1[], long a2[], long n, long m) {
+        ArrayList<Long> a=new ArrayList<>();
+       for(int i=0; i<a1.length; i++) {
+           a.add(a1[i]);
+       }
+       for(int i=0; i<a2.length; i++) {
+           if(a.contains(a2[i])) {
+                a.remove(Long.valueOf(a2[i]));
+           }
+           else {
+               return "No";
+           }
+       }
+       return "Yes";
+    }
+    
+    /***
+     *  USING HASHMAP
+     * @param a1
+     * @param a2
+     * @param n
+     * @param m
+     * @return
+     */
+    public static String isSubset(long a1[], long a2[], long n, long m) {
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(a1[i])){
+                map.put(a1[i], map.get(a1[i])+1);
+            } else {
+                map.put(a1[i], 1);
+            }
+        }
+        boolean isSubset = true;
+        for(int i=0;i<m;i++){
+            if(!map.containsKey(a2[i])){
+                isSubset = false;
+                break;
+            } else {
+                if(map.get(a2[i]) >= 1){
+                    map.put(a2[i], map.get(a2[i])-1);
+                } else{
+                    isSubset = false;
+                    break;
+                }
+            }
+        }
+        if(isSubset){
+            return "Yes";
+        }
+        return "No";
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextLong();
+        long [] arr = new long[100000];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextLong();
+        }
+        long m = scan.nextLong();
+        long [] brr = new long[100000];
+        for(int i=0;i<m;i++){
+            brr[i] = scan.nextLong();
+        }
+        String isSubset = isSubset(arr, brr, n, m);
+        if(isSubset.equals("Yes")){
+            System.out.println("It is a subset");
+        } else {
+            System.out.println("Its not a subset");
+        }
+        scan.close();
+    }
+}
