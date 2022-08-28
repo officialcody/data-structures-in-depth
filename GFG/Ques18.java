c85a7e3bfbad7aa62c6008e299e292d931b86466
officialcody <sparshgoelt26@gmail.com>
2022-08-28 14:04:15 +0530

    Reducing Walls

---
+++ GFG/Ques18.java
@@ -0,0 +1,29 @@
+package GFG;
+
+// Reducing Walls
+import java.util.*;
+
+class Ques18 {
+    public static int ReducingWalls (int arr[], int n, int k) {
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] > k){
+                count += (arr[i]-1)/ k;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] arr = new int[100000];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        int k = scan.nextInt();
+        int answer = ReducingWalls(arr, n, k);
+        System.out.println("Minimum number of times required to use superpower: " + answer);
+        scan.close();
+    }
+}
