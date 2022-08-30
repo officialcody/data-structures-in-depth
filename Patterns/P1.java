e33ef7b626b2dfd385e67b3623387a5d1160f544
officialcody <sparshgoelt26@gmail.com>
2022-08-30 11:31:17 +0530

    Hollow Triangle modified

--- Patterns/P1.java
+++ Patterns/P1.java
@@ -2,19 +2,13 @@ package Patterns;
 
 public class P1 {
     // Hollow Rectangle
-    public static void printHollowRectanglePattern(int n){
-        for(int i=0;i<n;i++){
-            if(i == 0 || i == n-1){
-                for(int j=0;j<=n;j++){
+    public static void printHollowRectanglePattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j == 0 || j == n || i == 0 || i == n - 1) {
                     System.out.print("*");
-                }
-            } else {
-                for(int j=0;j<=n;j++){
-                    if(j == 0 || j == n){
-                        System.out.print("*");
-                    } else {
-                      System.out.print(" ");
-                    }
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
