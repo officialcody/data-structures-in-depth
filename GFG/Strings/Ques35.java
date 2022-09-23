689833abce2d8265e461dbf815f010d975b5a775
officialcody <sparshgoelt26@gmail.com>
2022-09-23 23:48:07 +0530

    Repeated Character modified

--- GFG/Strings/Ques35.java
+++ GFG/Strings/Ques35.java
@@ -20,6 +20,21 @@ public class Ques35 {
         return answer;
     }
 
+    public static char firstRep2(String S) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+        for(int i=0;i<S.length();i++){
+            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0)+1);
+        }
+        char answer = '#';
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 1) {
+               answer = entry.getKey();
+               break;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String str = scan.next();
