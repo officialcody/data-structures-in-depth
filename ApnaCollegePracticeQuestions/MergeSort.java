fd7a5b8d6d6ffa22a7e035c9e4d23c24e27e52f3
officialcody <sparshgoelt26@gmail.com>
2022-11-09 22:38:01 +0530

    Modified merge sort

--- ApnaCollegePracticeQuestions/MergeSort.java
+++ ApnaCollegePracticeQuestions/MergeSort.java
@@ -11,34 +11,82 @@ public class MergeSort {
         merge(arr, startingIndex, middleIndex, endingIndex);
     }
 
+    // public static void merge(int [] arr, int startingIndex, int middleIndex, int endingIndex) {
+    //     int temp [] = new int[endingIndex-startingIndex+1];
+    //     int start = startingIndex;
+    //     int end = middleIndex+1;
+    //     int pos = 0;
+    //     while(start <= middleIndex && end <= endingIndex) {
+    //         if(arr[start] < arr[end]) {
+    //             temp[pos] = arr[start];
+    //             start++;
+    //         } else {
+    //             temp[pos] = arr[end];
+    //             end++;
+    //         }
+    //         pos++;
+    //     }
+    //     // If left part has more elements
+    //     while(start <= middleIndex) {
+    //         temp[pos++] = arr[start++];
+    //     }
+
+    //     // If right part has more elements
+    //     while(end <= middleIndex) {
+    //         temp[pos++] = arr[end++];
+    //     }
+
+    //     // copy temp to original array
+    //     for(pos = 0, start = startingIndex; pos < temp.length;pos++,start++){
+    //         arr[start] = temp[pos];
+    //     }
+    // }
+
     public static void merge(int [] arr, int startingIndex, int middleIndex, int endingIndex) {
-        int temp [] = new int[endingIndex-startingIndex+1];
-        int start = startingIndex;
-        int end = middleIndex+1;
-        int pos = 0;
-        while(start <= middleIndex && end <= endingIndex) {
-            if(arr[start] < arr[end]) {
-                temp[pos] = arr[start];
-                start++;
+        
+        int length1 = middleIndex - startingIndex + 1;
+        int length2 = endingIndex - middleIndex;
+
+        int [] first = new int[length1];
+        int [] second = new int[length2];
+
+        // copy values
+        int pos = startingIndex;
+        for(int i=0;i<length1;i++) {
+            first[i] = arr[pos];
+            pos++;
+        }
+        pos = middleIndex+1;
+        for(int i=0;i<length2;i++) {
+            second[i] = arr[pos];
+            pos++;
+        }
+
+        // merge 2 sorted arrays
+        int index1= 0;
+        int index2 = 0;
+        pos = startingIndex;
+        while(index1 < length1 && index2 < length2) {
+            if(first[index1] < second[index2] ) {
+                arr[pos] = first[index1];
+                index1++;
             } else {
-                temp[pos] = arr[end];
-                end++;
+                arr[pos] = second[index2];
+                index2++;
             }
             pos++;
         }
-        // If left part has more elements
-        while(start <= middleIndex) {
-            temp[pos++] = arr[start++];
+
+        while(index1 < length1) {
+            arr[pos] = first[index1];
+            index1++;
+            pos++;
         }
 
-        // If right part has more elements
-        while(end <= middleIndex) {
-            temp[pos++] = arr[end++];
-        }
-
-        // copy temp to original array
-        for(pos = 0, start = startingIndex; pos < temp.length;pos++,start++){
-            arr[start] = temp[pos];
+        while(index2 < length2) {
+            arr[pos] = second[index2];
+            index2++;
+            pos++;
         }
     }
 
